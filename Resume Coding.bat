1d4cc87a2da4e9fa40352641d6a1cc9517662cb4
siddiqui-m <muhammad.siddiqui98@hotmail.com>
2025-10-29 16:30:33 +0500

    Add files via upload

---
+++ Resume Coding.bat
@@ -0,0 +1,6 @@
+@echo off
+Title Coding Resumer
+Start /d "C:\Users\Muhammad Abdullah\AppData\Local\Programs\Microsoft VS Code" Code.exe
+Start https://learn.microsoft.com/en-gb/training/modules/csharp-if-elseif-else/2-exercise-if
+Start https://www.freecodecamp.org/learn/foundational-c-sharp-with-microsoft/create-and-run-simple-c-sharp-console-applications/add-decision-logic-to-your-code-using-if-else-and-else-if-statements-in-c-sharp
+Exit
---
+++ Resume Finance.bat
@@ -0,0 +1,12 @@
+@echo off
+Title Finance Resumer
+Start https://academy.binance.com/en
+Start https://platform.worldquantbrain.com/simulate
+Start https://learndashboard.cfainstitute.org/dashboard/in-progress
+Start https://study.cfainstitute.org/app/cfa-program-level-i-prerequisite-readings-for-2025
+Start https://docs.google.com/spreadsheets/d/1BuZL2PxDLwaCww7BsxMt0k10xrVSC4hdWXt0HDAF5bM/
+Start /wait https://docs.google.com/forms/d/e/1FAIpQLSfE4JutLdsvqHWgF8SyhEmJ-4UDcswIfWc4kyKNkUke-JLj1g/viewform
+Start /wait https://drive.google.com/drive/folders/17oCulnu6_u9_7t8JAFgg1hzMfmnCCPrV
+Start /d shell:Desktop /max /wait firefox.exe https://campusinsight.net:670/
+Exit 02
+Exit
